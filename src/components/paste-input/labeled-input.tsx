e49cd9d2eb9cc24119806c653dda4bf858394e98
dmitry-kulak <dmitry.kulak11@gmail.com>
2023-04-23 19:28:48 +0300

    paste submit and optional settings

---
+++ src/components/paste-input/labeled-input.tsx
@@ -0,0 +1,17 @@
+type LabeledInputProps = { label: string; name: string };
+
+const LabeledInput = ({ label, name }: LabeledInputProps) => (
+  <div className="grid grid-cols-3">
+    <label className="col-span-1" htmlFor={name}>
+      {label}
+    </label>
+
+    <input
+      className="col-span-2 bg-zinc-800 px-2 py-1 text-slate-100 outline-none"
+      type="text"
+      name={name}
+    />
+  </div>
+);
+
+export default LabeledInput;
--- src/components/paste-input/paste-input.tsx
+++ src/components/paste-input/paste-input.tsx
@@ -1,22 +1,81 @@
-import { type ChangeEventHandler } from "react";
+import type { FormEventHandler, ChangeEventHandler } from "react";
+import { useEffect, useState } from "react";
+import { z } from "zod";
 
+import LabeledInput from "./labeled-input";
+
+// resizes text area to fit text
 const autoResize: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
   e.target.style.height = "inherit";
   e.target.style.height = `${e.target.scrollHeight}px`;
 };
 
+const PasteSchema = z.object({
+  paste: z.string().nonempty("Must not be empty!"),
+  name: z
+    .string()
+    .trim()
+    .transform((name) => (name === "" ? undefined : name))
+    .optional(),
+});
+
 const PasteInput = () => {
+  const [emptyPasteError, setEmptyPasteError] = useState("");
+
+  useEffect(() => {
+    if (emptyPasteError) {
+      window.scrollTo(0, 0);
+    }
+  }, [emptyPasteError]);
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(e.currentTarget));
+    const result = PasteSchema.safeParse(formData);
+    if (result.success) {
+      setEmptyPasteError("");
+      console.log(result.data);
+      // submit
+    } else {
+      const errors = result.error.format();
+
+      const newEmptyPasteError =
+        errors.paste?._errors.join(", ") ?? emptyPasteError;
+      setEmptyPasteError(newEmptyPasteError);
+      newEmptyPasteError && window.scrollTo(0, 0);
+    }
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <h1 className="text-lg font-semibold">New Paste</h1>
-      <textarea
-        className="min-h-[300px] w-full resize-none bg-zinc-800 px-2 py-1 font-mono text-slate-100 outline-none"
-        onChange={autoResize}
-        spellCheck={false}
-        autoComplete="off"
-        autoCorrect="off"
-      />
-    </div>
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-lg font-semibold">New Paste</h1>
+
+        {emptyPasteError && (
+          <div className="bg-red-600 p-2">{emptyPasteError}</div>
+        )}
+
+        <textarea
+          className="min-h-[300px] w-full resize-none bg-zinc-800 px-2 py-1 font-mono text-slate-100 outline-none"
+          onChange={autoResize}
+          spellCheck={false}
+          autoComplete="off"
+          autoCorrect="off"
+          name="paste"
+        />
+      </div>
+
+      <div className="my-4 flex flex-col gap-2">
+        <h2 className="font-semibold">Optional Paste Settings</h2>
+        <div className="flex max-w-xs flex-col gap-4">
+          <LabeledInput label="Paste name" name="name" />
+          <button className="place-self-start" type="submit">
+            Create new Paste
+          </button>
+        </div>
+      </div>
+    </form>
   );
 };
 
